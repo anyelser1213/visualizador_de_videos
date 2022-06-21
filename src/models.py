022be164e205bd688d783bb63d5576ec1eb45dd8
Parley Sport <parleysport@gmail.com>
2022-06-21 13:06:36 -0400

    ya se borrar las categorias y subcategorias

--- src/models.py
+++ src/models.py
@@ -1,9 +1,11 @@
 import os
+import shutil #libreria para borrar carpetas esten o no llenas
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_delete, post_delete
+
 
 # Create your models here.
 
@@ -168,7 +170,7 @@ class Categoria(models.Model):
 
         print("Probando con categorias") 
         print(self.nombre)
-        os.mkdir(os.path.join(settings.MEDIA_ROOT)+"videos/"+self.nombre)
+        os.mkdir(os.path.join(settings.MEDIA_ROOT)+"/"+"videos/"+self.nombre)
         
         #self.video.name = self.categoria,"/",self.mes
         #aux = os.path.join(self.categoria,self.mes,self.video.name)
@@ -182,10 +184,41 @@ class Categoria(models.Model):
         
         super(Categoria, self).save(*args, **kwargs)
 
+
+
+#Funcion de señales en Django
+@receiver(pre_delete,sender=Categoria)
+def BorrarCategoria(sender,instance,**kwargs):
+
+    print(sender)
+    print(instance)
+    print("categoria: ",instance.nombre)
+
+
+    try:
+        #Para borrar el directorio y todo lo que haya dentro
+        ruta = os.path.join(settings.MEDIA_ROOT)+"/videos/"+instance.nombre
+        shutil.rmtree(ruta)
+
+        print(ruta)
+    except OSError as e:
+        print(f"Error:{ e.strerror}")
+    #eliminando con la ruta correcta
+    #print("RUTA: ",os.path.join(settings.MEDIA_ROOT+instance.video.name))
+    #print("RUTA MEDIA_ROOT: ",settings.MEDIA_ROOT)
+    #os.remove(os.path.join(settings.MEDIA_ROOT+instance.video.name))
+    #pathExt = r"/media/videos/zulia/enero/2021-03-22_21-01-22.mkv"
+    #os.remove(os.path.join(settings.MEDIA_ROOT)+"/"+"videos/"+instance.categoria.nombre+"/"+instance.nombre)
+    #os.remove(os.path.join(settings.MEDIA_ROOT+"\\media\\videos\\zulia\\enero\\2021-03-22_21-01-22.mkv"))
+
+    print("Se acaba de Borrar una Categoria jajaja")
+
+
 class Subcategoria(models.Model):
 
 
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200,unique=True)
+    categoria = models.ForeignKey("Categoria", on_delete=models.CASCADE)
     activo = models.BooleanField(default=True)
 
     
@@ -197,6 +230,56 @@ class Subcategoria(models.Model):
     def __str__(self):
         return self.nombre
 
+
+
+    #Editando los metodos
+    def save(self, *args, **kwargs): 
+
+        print("Probando con subcategorias") 
+        print(self.nombre)
+
+        os.mkdir(os.path.join(settings.MEDIA_ROOT)+"/"+"videos/"+self.categoria.nombre+"/"+self.nombre)
+        
+        #self.video.name = self.categoria,"/",self.mes
+        #aux = os.path.join(self.categoria,self.mes,self.video.name)
+        #print(aux)
+        #self.video.name = aux
+
+
+        #indice_final = 
+        #print(self.video.name)
+        #print("prueba: ",self.video.name[:self.video.name.index('.')])
+        
+        super(Subcategoria, self).save(*args, **kwargs)
+
+
+#Funcion de señales en Django
+@receiver(pre_delete,sender=Subcategoria)
+def BorrarSubacategoria(sender,instance,**kwargs):
+
+    print(sender)
+    print(instance)
+    print("categoria: ",instance.categoria.nombre)
+    print("subcategoria: ",instance.nombre)
+
+
+    try:
+        #Para borrar el directorio y todo lo que haya dentro
+        ruta = os.path.join(settings.MEDIA_ROOT)+"/videos/"+instance.categoria.nombre+"/"+instance.nombre
+        shutil.rmtree(ruta)
+
+        print(ruta)
+    except OSError as e:
+        print(f"Error:{ e.strerror}")
+    #eliminando con la ruta correcta
+    #print("RUTA: ",os.path.join(settings.MEDIA_ROOT+instance.video.name))
+    #print("RUTA MEDIA_ROOT: ",settings.MEDIA_ROOT)
+    #os.remove(os.path.join(settings.MEDIA_ROOT+instance.video.name))
+    #pathExt = r"/media/videos/zulia/enero/2021-03-22_21-01-22.mkv"
+    #os.remove(os.path.join(settings.MEDIA_ROOT)+"/"+"videos/"+instance.categoria.nombre+"/"+instance.nombre)
+    #os.remove(os.path.join(settings.MEDIA_ROOT+"\\media\\videos\\zulia\\enero\\2021-03-22_21-01-22.mkv"))
+
+    print("Se acaba de Borrar la subcategoria jajaja")
 
 
 
@@ -212,9 +295,7 @@ class Video(models.Model):
 
 
     nombre = models.CharField(max_length=200, null=True, blank=True)
-    
     categoria = models.ForeignKey("Categoria", on_delete=models.CASCADE)
-    #categoria = models.CharField(    max_length=50,    choices=categorias,    default="zulia",)
     subcategoria = models.ForeignKey("Subcategoria", on_delete=models.CASCADE, null=True, blank=True)
     activo = models.BooleanField(default=True)
     video = models.FileField(upload_to='videos/')
@@ -243,7 +324,7 @@ class Video(models.Model):
         #print("prueba: ",self.video.name[:self.video.name.index('.')])
         
         self.nombre = self.video.name[:self.video.name.index('.')]#Guardamos el nombre del video
-        self.video.name = os.path.join(str(self.categoria),str(self.mes),self.video.name)#Guardamos la ruta
+        self.video.name = os.path.join(str(self.categoria),str(self.subcategoria),self.video.name)#Guardamos la ruta
         super(Video, self).save(*args, **kwargs)
 
     
@@ -261,7 +342,27 @@ def BorrarVideo(sender,instance,**kwargs):
     print(sender)
     print(instance)
     print(instance.video)
-    print(instance.video.name)
+
+
+    print(sender)
+    print(instance)
+    print("categoria: ",instance.categoria.nombre)
+    print("subcategoria: ",instance.nombre)
+
+
+    try:
+        #Para borrar el directorio y todo lo que haya dentro
+        #ruta = os.path.join(settings.MEDIA_ROOT)+"/videos/"+instance.categoria.nombre+"/"+instance.nombre
+        #shutil.rmtree(ruta)
+        pass
+        #print(ruta)
+    except OSError as e:
+        
+        print(f"Error:{ e.strerror}")
+
+
+
+    #print(instance.video.nombre)
     #eliminando con la ruta correcta
     #print("RUTA: ",os.path.join(settings.MEDIA_ROOT+instance.video.name))
     #print("RUTA MEDIA_ROOT: ",settings.MEDIA_ROOT)
